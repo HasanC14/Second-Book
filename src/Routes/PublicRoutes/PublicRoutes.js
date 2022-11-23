5ad05528377d9868736f1980838126b0f25c9ed6
HasanC14 <chowdhuryh999@gmail.com>
2022-11-24 03:30:23 +0600

    Pages and Routes Added

---
+++ src/Routes/PublicRoutes/PublicRoutes.js
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      { path: "/", element: <Home></Home> },
+      { path: "/Login", element: <Login></Login> },
+      { path: "/Register", element: <Register></Register> },
+    ],
+  },
+]);
+export default router;
