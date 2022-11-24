038ff0197a9363b039b52905a8b3788173b2e399
HasanC14 <chowdhuryh999@gmail.com>
2022-11-25 04:32:49 +0600

    Seller my product added

--- src/Pages/DashboardPages/MyProducts.js
+++ src/Pages/DashboardPages/MyProducts.js
@@ -1,7 +1,59 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../Context/AuthProvider";
+import Loading from "../Shared/Loading/Loading";
 
 const MyProducts = () => {
-  return <div></div>;
+  const { User } = useContext(AuthContext);
+  // const [products, setProducts] = useState
+  const { data: products, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/SellerProducts/?email=${User?.email}`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+  console.log(products);
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+  return (
+    <div className="overflow-x-auto">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>Product ID</th>
+            <th>Product Name</th>
+            <th>Posted on</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr>
+              <th>{product._id}</th>
+              <td>{product.BookName}</td>
+              <td>
+                {product?.Time.split("T")[1].split(":")[0]}.
+                {product?.Time.split("T")[1].split(":")[1]} ({" "}
+                {product?.Time.split("T")[0]})
+              </td>
+              <td>
+                <button className="btn">Advertise</button>
+              </td>
+              <td>
+                <button className="btn">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default MyProducts;
